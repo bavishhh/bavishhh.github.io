5d41617f53c83c1de473e61bff767c4a927e68c7
bavishhh <31406183+bavishhh@users.noreply.github.com>
2025-12-22 06:12:00 +0000

    Deploying to gh-pages from @ bavishhh/bavishhh.github.io@207a57a459d16cd38de24d716abfaebd0f73f728 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/https:/drive.google.com/file/d/1wsyyaLyFbQJ5PbVNTJGxfuDrFuvgvDsm/view?usp=sharing";
+            window.location.href = "/https:/drive.google.com/file/d/1c9phRu7uOtZVlAmDyxJpnhDUCa352JxZ/view?usp=drive_link";
           },
         },{id: "nav-blog",
           title: "blog",
